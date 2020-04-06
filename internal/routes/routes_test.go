d85d3c6795af686bec0c8718a76aa15af2dcb58e
pserrano <pserrano@uolinc.com>
2020-04-06 14:24:19 -0300

    Adicionando testes de integração e melhoria rota para validação de senha

---
+++ internal/routes/routes_test.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func performRequest(r http.Handler, method, path string, body io.Reader) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, body)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestHandlerPasswordValidCases(t *testing.T) {
+
+	router := SetupRouter()
+	okCases := []string{"AbTp9!foo" , "XyzkJ#123", "178Mn@fcb", "SwROTS3!%&*" }
+
+	for _, v := range okCases {
+		w := performRequest(router, "POST", "/users/passwords/validations/isValid", strings.NewReader(v))
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, "true", w.Body.String())
+	}
+
+}
+
+func TestHandlerPasswordInvalidCases(t *testing.T) {
+
+	router := SetupRouter()
+	nokCases := []string{"A", "1", "A1b", "1A2b3C4D", "@" }
+	for _, v := range nokCases {
+		w := performRequest(router, "POST", "/users/passwords/validations/isValid", strings.NewReader(v))
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, "false", w.Body.String())
+	}
+
+}
